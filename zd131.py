5bf7e5ba3df6560e0f03b2620796e65b8b05c590
Kirpich <ponchik34@tuta.io>
2024-10-03 17:33:25 +0300

    lst

--- zd131.py
+++ zd131.py
@@ -9,13 +9,23 @@ num = 0
 #number oldest men in house
 
 sr = 0
+zp = -1
 
 
 for i in range(1, dl + 1, 2):
     if data_split[i+1] == '1':
         num = (i+1) // 2
-        if int(data_split[num*2 - 1]) > sr:
-            sr = num
-print(sr)
+        if int(data_split[(num * 2) - 1]) > sr:
+            sr = int(data_split[(num * 2) - 1])
+            zp = num
+
+
+
+output_data = open('output.txt', 'w')
+output_data.write(str(zp))
+#запись
+
 data_s.close()
+output_data.close()
+#ОБЯЗАТЕЛЬНО не забывать
 
